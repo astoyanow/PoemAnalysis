868c22d3651c06fc7454fecac77560df1d193c6a
Andrei Stoyanow <astoyanow13@gmail.com>
2020-04-23 17:08:33 -0500

    Restructured into class with functions

--- AnalysisCode/SentimentAnalysis.py
+++ AnalysisCode/SentimentAnalysis.py
@@ -6,26 +6,43 @@ from afinn import Afinn
 import os
 import re
 
-testfilepath = "/users/andrei/documents/github/poemanalysis/poemscorpus/1900"
-poems = []
-poemNames = []
-poemDict = {}
-afinn = Afinn()
-aScore = 0
-SID = SentimentIntensityAnalyzer()
-SIDScore = 0
+class SentimentAnalysis():
+    def __init__(self):
+        self.testfilepath = "/users/andrei/documents/github/poemanalysis/poemscorpus/1900"
+        self.poems = []
+        self.poemNames = []
+        self.poemDict = {}
+        self.afinn = Afinn()
+        self.aScore = 0
+        self.SID = SentimentIntensityAnalyzer()
+        self.SIDScore = 0
 
-for poem in os.listdir(testfilepath):
-    if poem.endswith(".txt"):
-        poemNames.append(poem[:-4].replace("_", " "))
-        fopen = open(testfilepath + "/" + poem)
-        poems.append(re.sub(r'[^\w\s]', '', fopen.read().lower().replace("\n", " ")))
-        fopen.close()
+    def openPoems(self):
+        for poem in os.listdir(self.testfilepath):
+            if poem.endswith(".txt"):
+                self.poemNames.append(poem[:-4].replace("_", " "))
+                fopen = open(self.testfilepath + "/" + poem)
+                self.poems.append(re.sub(r'[^\w\s]', '', fopen.read().lower().replace("\n", " ")))
+                fopen.close()
 
-for i in range(0, len(poems)):
-    poemDict[poemNames[i]] = poems[i]
-for poem in poemDict:
-    line = tokenize.sent_tokenize(poemDict[poem])
+
+    def poemsToDict(self):
+        for i in range(0, len(self.poems)):
+            self.poemDict[self.poemNames[i]] = self.poems[i]
+
+
+    def tokenizePoems(self):
+        for poem in self.poemDict:
+            line = tokenize.sent_tokenize(self.poemDict[poem])
+            print(line)
+            self.poemDict[poem] = line
+
+
+SA = SentimentAnalysis()
+SA.openPoems()
+SA.poemsToDict()
+SA.tokenizePoems()
+"""
     score = afinn.score(line[0])
     aScore += score
     ss = SID.polarity_scores(line[0])
@@ -35,3 +52,4 @@ for poem in poemDict:
 print('\n')
 print("Compound Afinn Score: ", aScore)
 print("Compound SID Score: ", SIDScore)
+"""
